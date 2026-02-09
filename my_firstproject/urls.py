4640b974e25b89bbf5fc5857884e431af9f10add
vidhikaiwart <vidhikaiwart@gmail.com>
2026-02-09 23:33:08 +0530

    Create models

--- my_firstproject/urls.py
+++ my_firstproject/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),      
     path('contact/', views.contact, name='contact'),
-    path('', include('myapp.urls')),
+    path('myapp/', include('myapp.urls')),
 
     path("__reload__/", include("django_browser_reload.urls")),
 ]
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    SUBJECT_CHOICES = [
+        ('Math', 'Mathematics'),    
+        ('Science', 'Science'),
+        ('History', 'History'),
+        ('Literature', 'Literature'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='student_images/')
+    age = models.IntegerField()
+    email = models.EmailField()
+    type = models.CharField(max_length=20, choices=SUBJECT_CHOICES)
+
+    def __str__(self):
+        return self.name
